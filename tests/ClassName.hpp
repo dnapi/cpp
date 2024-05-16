7dce3a57ff290291bab8e0fc75d1286aeb461c97
Alexandr Pimikov <apimikov@student.hive.fi>
2024-05-16 15:32:35 +0300

    init lists

---
+++ tests/ClassName.hpp
@@ -0,0 +1,16 @@
+#ifndef CLASSNAME_HPP
+# define CLASSNAME_HPP
+
+#include <iostream>
+
+class ClassName{
+
+	public :
+		char	a1;
+		int		a2;
+		float	a3;
+		ClassName(char p1, int p2, float p3);
+		~ClassName(void);
+		void	boo(void);
+};
+#endif 
