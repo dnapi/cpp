43bf194b6b97b0d40accc6df1f3979a6b19cce8d
Alexandr Pimikov <apimikov@student.hive.fi>
2024-05-16 09:52:16 +0300

    touch

---
+++ tests/stdio.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int	main(void)
+{
+	char	buff[512];
+
+	std::cout << "Hellow World" <<std::endl;
+	std::cin >> buff;
+	std::cout << "buf = >>" << buff << "<<" <<std::endl;
+}
