e20e089f0eed7c7b65e690efaf614378975fb09b
Alexandr Pimikov <apimikov@student.hive.fi>
2024-05-17 15:50:43 +0300

    getline is in use

--- m00/ex01/Contact.cpp
+++ m00/ex01/Contact.cpp
@@ -1,4 +1,4 @@
-#include "ClassName.hpp"
+#include "Contact.hpp"
 
 Contact::Contact()
 {
--- m00/ex01/main.cpp
+++ m00/ex01/main.cpp
@@ -1,9 +1,15 @@
-#include "ClassName.hpp"
+#include "Contact.hpp"
 
 int	main(void)
 {
-	ClassName	instance('z', 5, 3.14f);
-
+	Contact c;
+	std::string str;
+	for (int i = 0; i < 3; i++)
+	{
+	    std::cout << "Please enter a line of text: ";
+    	std::getline(std::cin, str);
+	    std::cout << "You entered: " << str << std::endl;
+	}
 //	instance.foo = 42;
 //	std::cout << "foo = " << instance.foo << std::endl;
 //	instance.boo();
