a801bbd8034f11c1293a7df95ad4e998bed517be
Alexandr Pimikov <apimikov@student.hive.fi>
2024-05-24 10:57:50 +0300

    first verstion of Harl

---
+++ m01/ex05/Harl.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Harl{
+	public:
+		Harl(void);
+		~Harl(void);
+		void	printStr(std::string str) const;
+		void	complain(std::string level);
+	private :
+//		typedef	void	(Harl::*MemberFunction)(void);
+//		MemberFunction	_functions[5];
+		void	(Harl::*_functions[5])(void);
+		std::string _LevelList[5];
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+		void	silence(void);
+};
---
+++ m01/ex06/Harl.cpp
@@ -0,0 +1,56 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	_LevelList[0] = "DEBUG";
+	_LevelList[1] = "INFO";
+	_LevelList[2] = "WARNING";
+	_LevelList[3] = "ERROR";
+	_LevelList[4] = "WRONG TYPE OF COMPLAIN";
+	_functions[0] = &Harl::debug;
+	_functions[1] = &Harl::info;
+	_functions[2] = &Harl::warning;
+	_functions[3] = &Harl::error;
+	_functions[4] = &Harl::silence;
+}
+
+Harl::~Harl(void) {}
+
+void Harl::printStr(std::string str) const
+{
+	std::cout << str << std::endl
+			  << std::endl;
+}
+
+void Harl::debug(void)
+{
+	printStr("I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!");
+}
+
+void Harl::info(void)
+{
+	printStr("I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!");
+}
+
+void Harl::warning(void)
+{
+	printStr("I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.");
+}
+
+void Harl::error(void)
+{
+	printStr("This is unacceptable! I want to speak to the manager now.");
+}
+
+void Harl::silence(void){
+	printStr("No comments. Allowed types of complains are DEBUG, INFO, WARNING and ERROR");
+}
+
+void Harl::complain(std::string level)
+{
+	int i = 0;
+	while (i < 4 && level != _LevelList[i])
+		i++;
+	std::cout << "[ " << _LevelList[i] << " ]" << std::endl;
+	(this->*_functions[i])();
+}
