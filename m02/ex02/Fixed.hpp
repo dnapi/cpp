a5ba74c4ea311083797341a8102cdbc8d565b12a
Alex <pimikov@gmail.com>
2024-05-27 07:34:31 +0300

    ex02 started

---
+++ m02/ex02/Fixed.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	public:
+		// constructors
+		Fixed();
+		Fixed(Fixed const &number);
+		Fixed(const int	&number);
+		Fixed(const float &number);
+
+		//destructors
+		~Fixed();
+
+		//operators
+		Fixed	&operator=(const Fixed &number);
+
+		//setter
+		void		setRawBits(int const raw);
+
+		//getter
+		int	getRawBits(void) const;
+		
+		// other member functions
+		float	toFloat(void) const;
+		int	toInt(void) const;
+
+
+	private:
+		int			_value;
+		static const int	_fract_bits;
+};
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &number);
+
+#endif
