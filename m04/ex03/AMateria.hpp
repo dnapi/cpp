f46be2add4569a932f9372420622f2985ecb017c
Alex <pimikov@gmail.com>
2024-06-03 07:31:18 +0300

    touched

--- m04/ex03/AMateria.hpp
+++ m04/ex03/AMateria.hpp
@@ -1,12 +1,13 @@
 class AMateria
 {
 	protected:
+		std::string const& type
+	public:
 		AMateria();
-    public:
-		AMateria(std::string const & type);
 		~AMateria();
-		[...]
-		std::string const & getType() const; //Returns the materia type
+		AMateria(std::string const& type);
+		AMateria& operator=(AMateria& other);
+		std::string const& getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
 };
