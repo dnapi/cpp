3157586708a028c655fc8f0b528ef4716c4a359d
Alexandr Pimikov <apimikov@student.hive.fi>
2024-08-08 12:53:55 +0300

    minor

--- m06/ex00/ScalarConverter.cpp
+++ m06/ex00/ScalarConverter.cpp
@@ -15,8 +15,18 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter & other){
 	return *this;
 }
 
+void ScalarConverter::printChar(char c)
+{
+	std::cout << "char: ";
+	if (c)
+		std::cout << c;
+	else
+		std::cout << "Non displayable";
+}
+
 void ScalarConverter::print(char c, int i, float f, double d){
-	std::cout << "char: " << c 
+	printChar(c); 
+	std::cout 
 		<< "\nint: " << i
 		<< "\nfloat: " << f 
 		<< "\ndouble: " << d 
@@ -24,8 +34,11 @@ void ScalarConverter::print(char c, int i, float f, double d){
 }
 
 bool ScalarConverter::isChar(const std::string & str){
-	if (!(str.size() != 1 && isalpha(str[0])))
+	if (str.size() != 1)
 		return false;
+	if (!isalpha(str[0]))
+		return false;
+	toChar(str);
 	return true;
 }
 
@@ -37,9 +50,59 @@ void ScalarConverter::toChar(const std::string & str){
 	print(c, i, f, d);
 }
 
+bool ScalarConverter::isInt(const std::string & str){
+	if (!((str[0] == '-') | isdigit(str[0])))
+		return false;
+	size_t pos = 0;
+	int i = 0;
+	try {
+		i = std::stoi(str, &pos);
+        	std::cout << "The int value is: " << i << std::endl;
+	}
+	catch (const std::invalid_argument& e) {
+        	std::cout << "Int: Invalid argument: " << e.what() << std::endl;
+		return false;
+	}
+	catch (const std::out_of_range& e) {
+        	std::cout << "Int: Out of range: " << e.what() << std::endl;
+		return false;
+    	}
+	if (pos != std::string::npos)
+	{
+		std::cout << "int remainings=" << str.substr(pos) << "\n";
+		return false;
+	}
+	char c = 0;
+	if (i > 31 && i < 127)
+	{
+		c = static_cast<char>(i);
+	}
+//	else (i < 0 || i > 127)
+
+	float f = static_cast<float>(i);
+	double d = static_cast<double>(i);
+	print(c, i, f, d);
+	return true;
+}
+
+void ScalarConverter::toInt(const std::string & str){
+	char c = str[0];
+	int i = static_cast<int>(c);
+	float f = static_cast<float>(c);
+	double d = static_cast<double>(c);
+	print(c, i, f, d);
+}
+
 void ScalarConverter::convert(const std::string str){
+	if (str.empty())
+	{
+		std::cout << "[ERROR] nothing to convert\n";
+		return ;
+	}
 	if (isChar(str))
-		toChar(str);
+		return ;
+	if (isInt(str))
+		return ;
 	size_t pos = 0;
 	try {
 		double num = std::stod(str, &pos);
--- m06/ex00/ScalarConverter.hpp
+++ m06/ex00/ScalarConverter.hpp
@@ -14,8 +14,11 @@ class ScalarConverter{
 		ScalarConverter(const ScalarConverter &);
 		ScalarConverter& operator=(const ScalarConverter &);
 		static void print(char, int, float, double);
+		static void printChar(char);
 		static bool isChar(const std::string&);
 		static void toChar(const std::string&);
+		static bool isInt(const std::string&);
+		static void toInt(const std::string&);
 };
 
 #endif
