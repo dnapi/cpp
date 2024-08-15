21ff88585a2ef75f1d02f6d0b494595460124a23
Alexandr Pimikov <pimikov@gmail.com>
2024-08-15 08:55:51 +0300

    touch

--- m08/ex00/easyfind.hpp
+++ m08/ex00/easyfind.hpp
@@ -1,3 +1,12 @@
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <deque>
+#include <list>
 #include <vector>
 
 //template <template<int> typename Container = std::vector>
@@ -8,3 +17,13 @@ typename T::const_iterator easyfind(T t, int i){
         throw std::exception();
     return it;
 }
+
+template <typename T>
+typename T::iterator easyfind(T t, int i){
+    T::iterator it = std::find(t.begin(), t.end(), i); 
+    if (it == t.end())
+        throw std::exception();
+    return it;
+}
+
+#endfi
