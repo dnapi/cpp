5eb9b3b0946963c8f6f32d961c955659e0621da8
Alexandr Pimikov <pimikov@gmail.com>
2024-08-27 07:00:11 +0300

    minor

--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: apimikov <apimikov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 09:05:24 by apimikov          #+#    #+#             */
-/*   Updated: 2024/08/27 06:42:59 by apimikov         ###   ########.fr       */
+/*   Updated: 2024/08/27 06:59:31 by apmikov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,26 @@
 #include <vector>
 #include <string>
 
+int setData(std::vector<int>& vec, int argc, char** argv);
+
 int main(int argc, char** argv){
     if (1 == argc){
         std::cerr << "Error. \n";
         return (1);
     }
     std::vector<int> vec;
+    if (setData(vec, argc, argv)){
+        std::cerr << "Error. \n";
+        return (1);
+    }
+    for (auto& i : vec){
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+    return 0;
+}
+
+int setData(std::vector<int>& vec, int argc, char** argv){
     size_t pos;
     std::string str;
     int value;
@@ -30,18 +44,12 @@ int main(int argc, char** argv){
             value = std::stoi(str, &pos);
         }
         catch (...){
-            std::cerr << "Error. \n";
             return (1);
         }
         if (pos != str.size() || value < 0){
-            std::cerr << "Error. \n";
             return (1);
         }
         vec.push_back(value);
     }
-    for (auto& i : vec){
-        std::cout << i << ' ';
-    }
-    std::cout << '\n';
-    return 0;
+	return 0;
 }
